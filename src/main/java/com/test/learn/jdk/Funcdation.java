25c9d872faf9721e5f1d707d02d0292a52d87428
曾志武 <zengzw1220@13322ty.com>
2018-04-27 14:40:48 +0800

    push

---
+++ src/main/java/com/test/learn/jdk/Funcdation.java
@@ -0,0 +1,28 @@
+/*
+ * @Project Name: springmvcdemo
+ * @File Name: Funcdation.java
+ * @Package Name: com.test.learn.jdk
+ * @Date: 2018年4月27日下午2:38:34
+ * @Creator: zengzw-1220
+ * @line------------------------------
+ * @修改人: 
+ * @修改时间: 
+ * @修改内容: 
+ */
+
+package com.test.learn.jdk;
+
+/**
+ * TODO
+ * @author zengzw-1220
+ * @date 2018年4月27日下午2:38:34
+ * @see
+ */
+public class Funcdation {
+	
+	public static void main(String[] args) {
+
+		int max = Math.max(2, 3);
+		System.out.println(max);
+	}
+}
