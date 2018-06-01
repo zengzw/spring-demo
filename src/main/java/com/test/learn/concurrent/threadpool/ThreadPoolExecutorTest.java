28f93ed3643b9b698da51680c30f5e0a5588674e
曾志武 <zengzw1220@13322ty.com>
2018-06-01 16:46:34 +0800

    add readme content

--- src/main/java/com/test/learn/concurrent/threadpool/ThreadPoolExecutorTest.java
+++ src/main/java/com/test/learn/concurrent/threadpool/ThreadPoolExecutorTest.java
@@ -10,7 +10,7 @@
  * @修改内容: 
  */
 
-package com.test.learn.concurrent;
+package com.test.learn.concurrent.threadpool;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -23,13 +23,22 @@ import java.util.concurrent.TimeUnit;
  * @see
  */
 public class ThreadPoolExecutorTest {
-	public static void main(String[] args) {   
+	public static void main(String[] args) {  
+		
+		/*
+		 执行任务时 
+			1. 如果线程池中线程数量 < core，新建一个线程执行任务； 
+			2. 如果线程池中线程数量 >= core ,则将任务放入任务队列 
+			3. 如果线程池中线程数量 >= core 且 < maxPoolSize，且任务队列满了，则创建新的线程； 
+			4. 如果线程池中线程数量 > core ,当线程空闲时间超过了keepalive时，则会销毁线程；由此可见线程池的队列如果是无界队列，那么设置线程池最大数量是无效的；
+		 */
+		
 		//15个任务的情况下：5个线程执行，再进来的，放到队列里头；队列5个满了之后，如果还多的，会再创建5个临时线程来执行其中的5个任务。
 			ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 20, TimeUnit.SECONDS,
 				new ArrayBlockingQueue<Runnable>(5));
 
 		//LinkedBlockingQueue  线程数只有5个，多余的任务添加到队列当中。当线程执行完了，再从队列中拿数据执行。
-	/*	ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 20, TimeUnit.SECONDS,
+	/*	ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 5, 20, TimeUnit.SECONDS,
 				new LinkedBlockingQueue<Runnable>());
 */
 
@@ -38,7 +47,6 @@ public class ThreadPoolExecutorTest {
 		/*ThreadPoolExecutor executor = new ThreadPoolExecutor(0, 20, 20, TimeUnit.SECONDS,
 				new SynchronousQueue<Runnable>());*/
 
-		
 
 		for(int i=0;i< 15;i++){
 			MyTask myTask = new MyTask(i);
@@ -51,17 +59,7 @@ public class ThreadPoolExecutorTest {
 
 		//executor.shutdown();
 
-		while(true){
-			try {
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println("poolSize:"+executor.getPoolSize() +" queueSize:"+executor.getQueue().size());
 		}
-
-	}
 }
 
 class MyTask implements Runnable {
