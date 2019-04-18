b6c2ec02824d2d99753c5290bd3169a93ceb3213
曾志武 <zengzhiwu@163.com>
2019-04-18 12:20:45 +0800

    添加proxy

--- src/main/java/com/test/learn/proxy/cglib/CGLibProxy.java
+++ src/main/java/com/test/learn/proxy/cglib/CGLibProxy.java
@@ -12,28 +12,29 @@ import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 
 /**
+ *CGlib 代理类
  *
  * @author zengzw
  * @date 2017年6月29日
  */
 public class CGLibProxy implements MethodInterceptor{
 
-    
+
     public <T> T getProxy(Class<T> clz){
         return (T) Enhancer.create(clz,this);
     }
-    
-    
+
+
     @Override
     public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
-        
-       System.out.println(method.getName());
-        
+
+        System.out.println(method.getName());
+
         System.out.println("cglib before");
         Object object = proxy.invokeSuper(obj, args);
         System.out.println("cglib after");
 
         return object;
-    } 
+    }
 
 }
